27ddc3316c77ce1d5ef9a859d948200b926dec33
Asher <1057488870@qq.com>
2016-09-03 03:39:02 +0800

    remove some comments and add some description

--- main.js
+++ main.js
@@ -2,35 +2,15 @@
 var fs = require('fs'),
     scene = require('./scene');
 
-//nodejs 上下文中的console.log跟browser里边的不一样所以把browser的log传进来用..
+
 module.exports = function (logger) {
     logger("img_data generating");
-     //var start;
-     //var end;
-     //start = new Date();
-    //var out = fs.createWriteStream(filePath);
-    // end = new Date();
-    // timeElasped(start, end, 'create stream');
 
-    // start = new Date();
     var current = scene.current;
     var instance = new current();
-    // end = new Date();
-    // timeElasped(start, end, 'create instance');
 
-    // start = new Date();
     var canvas = instance.canvas;
-
     var ret=canvas.toDataURL();
     logger("img_data generated");
     return ret;
-
-    //canvas.pngStream().pipe(out);
-
-    // timeElasped(start, end, 'write to stream');
 };
-
-// function timeElasped(start, end, name) {
-//     var elasped = end - start;
-//     console.log(name + ": " + elasped);
-// }
